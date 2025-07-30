e02fc971f7681c7e66bd74a9ce1a48e054945088
Marvin291-dev <marvin.velasquez@udeo.edu.gt>
2025-07-30 10:34:39 -0600

    Primer commit case 3

---
+++ Examen parte 2.py
@@ -0,0 +1,43 @@
+while True:
+    print("\nBienvenido")
+    print("1. Calcular el MCD de dos números")
+    print("2. Cadena repetida")
+    print("3. Contar cuantas veces aparece una letra en una cadena")
+    print("4. Convertir un numero decimal a binario")
+    print("5 Calcular cuantos dígitos tiene un número")
+    print("6. Salir")
+    try:
+        opcion = int(input("Ingresa una opcion: "))
+        match opcion:
+            case 1:
+                print()
+            case 2:
+
+            case 3:
+                def Contar_Palabra(Palabra, letra):
+                    if not Palabra:
+                        return 0
+                    elif Palabra[0] == letra:
+                        return 1 + Contar_Palabra(Palabra[1:], letra)
+                    else:
+                        return Contar_Palabra(Palabra[1:], letra)
+
+                palabra = input("Ingresa una palabra: ")
+                letra = input("Ingresa una letra que quieres contar: ").strip()
+
+                if len(letra) != 1:
+                    print("Por favor ingrese una letra")
+                else:
+                    Resultado = Contar_Palabra(palabra, letra)
+                    print(f"la letra {letra} aparece {Resultado} vecen en la palabra: {palabra}")
+            case 4:
+                print()
+            case 5:
+                print()
+            case 6:
+                print("Vuelva Pronto")
+                break
+            case _:
+                print("Opcion invalida, intente de nuevo")
+    except ValueError:
+        print("Por favor ingrese un numero que sea de 1 a 6")
